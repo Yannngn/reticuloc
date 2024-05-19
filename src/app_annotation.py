9591b5135aefa7f610a5ac0fd1cfa2c71e5390e4
desktop_yannngn_ubuntu <yann.nicholas@gmail.com>
2024-05-19 18:42:52 -0300

    adding fastapi middleware

--- src/app_annotation.py
+++ src/app_annotation.py
@@ -2,29 +2,36 @@ import base64
 import binascii
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 from results import ResultsJsonEncoder
 from reticulai import ReticulAI
 
-# from pydantic import BaseModel
-
-
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
-# class ImageData(BaseModel):
-#     image_data: str
+
+class ImageData(BaseModel):
+    image_data: str
 
 
 @app.post("/detect")
-async def detect(image_data: str) -> list[dict]:
+async def detect(image_data: ImageData) -> list[dict]:
     detector = ReticulAI()
 
     try:
-        _ = base64.b64decode(image_data, validate=True)
+        _ = base64.b64decode(image_data.image_data, validate=True)
     except binascii.Error as e:
         raise HTTPException(status_code=400, detail=f"{e} Invalid image data")
 
-    result = detector.detect(image_data)
+    result = detector.detect(image_data.image_data)
 
     return ResultsJsonEncoder.response_with_boxes(result, detector.names)
