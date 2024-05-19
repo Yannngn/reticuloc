6b2a413fa8afadba16636185636900553332388c
desktop_yannngn_ubuntu <yann.nicholas@gmail.com>
2024-05-19 19:03:13 -0300

    Update yolov8n and yolov9c model paths in training scripts

---
+++ yolov9_train.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ubuntu_yannn/projects/Yannngn/reticuloc
+source .venv/bin/activate
+yolo detect train model=checkpoints/yolov9c.pt data=data/dataset.yaml project=runs name=v9 imgsz=320 rect=True hsv_h=0.0 hsv_s=0.0 hsv_v=0.0 degrees=0.0 translate=0.0 scale=0.0 shear=0.0 perspective=0.0 flipud=0.0 fliplr=0.0 bgr=0.0 mosaic=0.0
--- yolov9_train_aug.sh
+++ yolov9_train_aug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu_yannn/projects/Yannngn/reticuloc
 source .venv/bin/activate
-yolo detect train model=yolov9c.pt data=data/dataset.yaml project=runs name=aug_v9 imgsz=320 rect=True hsv_h=0.0 hsv_s=0.0 hsv_v=0.0 degrees=15 translate=0.1 scale=0.5 shear=0.0 perspective=0.0 flipud=0.5 fliplr=0.5 bgr=0.0 mosaic=0.0
+yolo detect train model=checkpoints/yolov9c.pt data=data/dataset.yaml project=runs name=aug_v9 imgsz=320 rect=True hsv_h=0.0 hsv_s=0.0 hsv_v=0.0 degrees=15 translate=0.1 scale=0.5 shear=0.0 perspective=0.0 flipud=0.5 fliplr=0.5 bgr=0.0 mosaic=0.0
