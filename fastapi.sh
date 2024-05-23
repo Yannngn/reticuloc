153f43e7942769294ff49822e98c60eb62078f89
desktop_yannngn_ubuntu <yann.nicholas@gmail.com>
2024-05-22 22:46:15 -0300

    Update virtual environment activation path in training scripts

---
+++ fastapi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ubuntu_yannn/projects/Yannngn/reticuloc
+source /home/ubuntu_yannn/.cache/pypoetry/virtualenvs/reticuloc-mvAVDMvm-py3.11/bin/activate
+fastapi dev src/app/app.py
